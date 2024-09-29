95627fef85f15598d8cdf13eb0218eb78b5ff864
amitlunaich0 <139340624+amitlunaich0@users.noreply.github.com>
2024-09-29 21:39:30 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Network Engineer",
+    "Security Analysis",
+    "Video Editor",
+  ],
+  typeSpeed: 10,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
